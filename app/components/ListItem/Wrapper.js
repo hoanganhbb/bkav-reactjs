ce2a300f62377afb862f8e39c3974c7829f56d1b
VuongLong6669 <vuonglong2196@gmail.com>
2021-04-08 15:40:46 +0700

    fix food item

--- app/components/ListItem/Wrapper.js
+++ app/components/ListItem/Wrapper.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  width: 1220px;
+  margin: 0 auto;
   &.grid {
     display: grid;
     grid-template-columns: 33.3333% 33.3333% 33.3333%;
